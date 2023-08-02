c504102ed65e029fb0512bed76642754b47af941
yonnk <yonnk2019@163.com>
2023-08-03 07:21:43 +0800

    修改依赖和入口文件，run dev成功

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom";
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(
+  <BrowserRouter>
+      <I18nextProvider i18n={i18n}>
+          <App />
+      </I18nextProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
