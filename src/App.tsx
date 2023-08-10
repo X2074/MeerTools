5ec838df33c323dea28ce7eb45398e08d4f696f3
yonnk <yonnk2019@163.com>
2023-08-10 21:12:00 +0800

    rem实现以及窗口自适应

--- src/App.tsx
+++ src/App.tsx
@@ -21,13 +21,10 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#42a5f5',
-      // light: will be calculated from palette.primary.main,
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
+      main: '#31EB31',
     },
     secondary: {
-      main: '#0288d1',
+      main: '#090904',
       light: '#03a9f4',
       // dark: will be calculated from palette.secondary.main,
       contrastText: '#01579b',
