dc35a99b95ee440682e40ef77b1b8baaa330556d
typicalAlex <59535066+yonnk-run@users.noreply.github.com>
2023-10-16 14:33:45 +0800

    Update rem.js

--- src/utils/rem.js
+++ src/utils/rem.js
@@ -4,8 +4,7 @@ const baseSize = 100
 // 设置 rem 函数
 function setRem () {
   // 当前页面宽度相对于 1920 宽的缩放比例，可根据自己设计图的宽度修改。
-  // const scale = document.documentElement.clientWidth / 1440
-  const scale = window.outerWidth / 1440
+  const scale = document.documentElement.clientWidth / 1440
   // 设置页面根节点字体大小，比例 1/4 - 2
   document.documentElement.style.fontSize = (baseSize * Math.min(Math.max(0.25, scale), 2)) + 'px'
 }
