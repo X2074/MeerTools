bb10df402bd629806dc19a4839ed08739fd8e10c
wanglan <809382327@qq.com>
2024-12-05 17:02:59 +0800

    新增批量转账部分代码

---
+++ src/config/constants/types.ts
@@ -0,0 +1,14 @@
+export interface Token {
+    address: string;
+    name: string;
+    symbol: string;
+    decimals: number;
+    logoURI?: string;
+    chainId: number | undefined;
+}
+export interface Address {
+    // 97?: string;
+    // 56: string;
+    // 4: string;
+    8131: string;
+}
