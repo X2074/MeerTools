19f5e55692a49460e435f483fe5dfdcce699f86b
liman <1310742648@qq.com>
2024-12-10 17:30:15 +0800

    智能合约菜单

--- src/views/contractDeployment/index.vue
+++ src/views/contractDeployment/index.vue
@@ -6,4 +6,13 @@
 import { ref, onMounted, watch } from "vue";
 // ERC20合约
 import contractContent20 from "./components/contractContent20/index.vue";
+const contractTypes = [
+  { name: "ERC20", type: "" },
+  { name: "ERC721", type: "" },
+  { name: "ERC1155", type: "" },
+  { name: "Stablecoin*", type: "" },
+  { name: "Real-World Asset*", type: "" },
+  { name: "Governor", type: "" },
+  { name: "Custom", type: "" },
+];
 </script>
