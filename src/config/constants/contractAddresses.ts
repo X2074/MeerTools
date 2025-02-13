e442482436b484318ba714ed52e4d48022274ce6
wanglan <809382327@qq.com>
2025-02-13 15:33:41 +0800

    1、优化样式/修复文案 2、修改projectID 3、注释无用数据

--- src/config/constants/contractAddresses.ts
+++ src/config/constants/contractAddresses.ts
@@ -4,10 +4,10 @@ export default {
     multiTransfer: {
         [ChainId.QITMEER_TESTNET]: "0x4c4d6981c043c76c26413D761a217c95289353F1",
         [ChainId.QITMEER]: "0x4c4d6981c043c76c26413D761a217c95289353F1",
-        [ChainId.RINKEBY]: "",
-        [ChainId.KOVAN]: "",
-        [ChainId.GOERLI]: "",
-        [ChainId.BSC_TESTNET]: "",
-        [ChainId.BSC]: "",
+        // [ChainId.RINKEBY]: "",
+        // [ChainId.KOVAN]: "",
+        // [ChainId.GOERLI]: "",
+        // [ChainId.BSC_TESTNET]: "",
+        // [ChainId.BSC]: "",
     },
 };
