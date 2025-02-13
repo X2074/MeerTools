68f719ab4d320bd863c0bf500389a944a6ee91b3
wanglan <809382327@qq.com>
2025-02-13 23:35:43 +0800

    新增预设网络功能（测试其他功能，此处暂时注释）

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -3,15 +3,11 @@
 import { ref, onMounted, reactive, watch } from "vue";
 // 引入钱包链接模块
 import { useConnect, useAccount, useDisconnect } from "@wagmi/vue";
-import { config } from "@/wagmi";
-import { changeChain } from "@/utils/wallet";
 import { waitForWaltConnect } from "@/utils";
-import { DEFAULT_CHAINID } from "@/config/constants/constant";
+import chains from "@/config/constants/chains.json";
 import { useRouter } from "vue-router";
-import store from "@/store";
-import bus from "@/utils/bus.js";
-
-import Web3 from "web3";
+import { watchAccount } from "@wagmi/core";
+import { config } from "@/wagmi";
 const router = useRouter();
 // 钱包选项
 let loginWallt = ref(false);
@@ -25,9 +21,9 @@ let confirmLoading = ref(false);
 const { connect, connectors, error } = useConnect();
 // 获取登录信息模块
 const { address, chainId, status } = useAccount();
+import { changeChain } from "@/utils/wallet";
 // 退出登录
 const { disconnect } = useDisconnect();
-const account = useAccount();
 /* <p>水龙头</p>
 <p>批量转账</p>
 <p>智能合约自动部署</p>
@@ -71,15 +67,38 @@ const menus = [
     // },
     { name: "qitmeer", path: "https://qitmeer.io/" },
 ];
+// 下拉列表的网络
+let chainsData = reactive([]);
+// 预设选中的网络
+let chainIdCheck = ref(8131);
 const goPath = (path) => {
-    console.log(111);
     if (!path) return;
     if (path.startsWith("http")) {
         window.open(path);
     } else {
         router.push(path);
     }
-};
+}; // 监听账户切换操作
+watchAccount(config, {
+    onChange(res) {
+        if (res.isConnected) {
+            console.log(res, "resresresresres");
+            // 过滤，看是否是预设的网络
+            let changeChainId = chainsData.filter((item) => {
+                if (item.id === res.chainId) return item;
+            });
+            // 如果不是，就切换为预设网络
+            if (!changeChainId || !changeChainId.length) {
+                changeChain(chainIdCheck.value);
+            }
+        } else {
+            // 第一次获取的状态是false，此处需要做特殊处理，判断是否真的未登录状态
+            // if (store.getters.token !== '') {
+            // store.dispatch('user/logout')
+            // }
+        }
+    },
+});
 /**
  * 处理连接登录逻辑
  * @param {Object} data - 包含连接信息的对象
@@ -103,6 +122,13 @@ const connectLogin = (data) => {
 };
 
 onMounted(async () => {
+    console.log(chains, "chains");
+    chainsData = [
+        chains["QngMainnet"],
+        chains["QngTestnet"],
+        chains["AmanaTestnet"],
+    ];
+    console.log(chainsData, "chainsData");
     if (status.value == "connected") {
         isLogin.value = true;
     }
@@ -113,11 +139,26 @@ watch(
     async (newV) => {
         if (newV == "connected") {
             isLogin.value = true;
+            // 过滤，看是否是预设的网络
+            let changeChainId = chainsData.filter((item) => {
+                if (item.id === chainId.value) return item;
+            });
+            // 如果不是，就切换为预设网络
+            if (!changeChainId || !changeChainId.length) {
+                changeChain(chainIdCheck.value);
+            } else {
+                chainIdCheck.value = chainId.value;
+            }
         } else {
             isLogin.value = false;
         }
     }
 );
+
+const changeChainId = () => {
+    console.log(chainIdCheck.value, "chainIdCheck.value");
+    changeChain(chainIdCheck.value);
+};
 
 //省略地址中间内容
 const omit = (address: any, len: any) => {
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,14 +1,17 @@
 import { http, createConfig } from '@wagmi/core'
 import { walletConnect, injected } from '@wagmi/vue/connectors'
 import { CHAINS, DEFAULT_CHAINID, W3M_PROJECT_ID } from '@/config/constants/constant'
+import chains from '@/config/constants/chains.json'
 
 export const config = createConfig({
-	chains: CHAINS,
+	chains: [chains['QngMainnet'], chains['QngTestnet'], chains['AmanaTestnet']],
 	connectors: [
 		walletConnect({ projectId: W3M_PROJECT_ID }),
 		injected()
 	],
 	transports: {
-		[DEFAULT_CHAINID]: http()
+		[chains['QngMainnet']['id']]: http(),
+		[chains['QngTestnet']['id']]: http(),
+		[chains['AmanaTestnet']['id']]: http()
 	}
 })
