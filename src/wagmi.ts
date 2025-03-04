04ee175d117735b0fed7e8e2565fdb83755282be
wanglan <809382327@qq.com>
2025-03-04 17:56:51 +0800

    修复UI走查bug

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,15 +3,15 @@ import { walletConnect, injected } from '@wagmi/vue/connectors'
 import { W3M_PROJECT_ID } from '@/config/constants/constant'
 import chains from '@/config/constants/chains.json'
 
-export const config = createConfig({
-	chains: [chains['QngMainnet'], chains['QngTestnet'], chains['AmanaTestnet']],
+let chainData: any = {
+	chains: [chains['QngMainnet'], chains['QngTestnet']],
 	connectors: [
 		walletConnect({ projectId: W3M_PROJECT_ID, relayUrl: 'wss://relay.walletconnect.org', showQrModal: true, }),
 		injected()
 	],
 	transports: {
 		[chains['QngMainnet']['id']]: http(),
-		[chains['QngTestnet']['id']]: http(),
-		[chains['AmanaTestnet']['id']]: http()
+		[chains['QngTestnet']['id']]: http()
 	}
-})
+}
+export const config = createConfig(chainData)
